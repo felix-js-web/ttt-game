9a2eeb09071141e0ca99b7520d339033860b5986
FelixA <felix.java.web@gmail.com>
2024-07-09 21:45:24 +1000

    JIRA-3333 optimised all the flows minimal duplication and ifs and switches now using sets

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,16 @@
 package org.example;
 
 
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
     static Scanner input = new Scanner(System.in);
+
+    static int NUMBER_OF_SIDES = 3;
     static final char H = 'H';
     static final char C = 'C';
 
@@ -38,98 +42,109 @@ public class Main {
 
     private static void playerMove(char[][] gameBoard) {
 
-        System.out.println("Please make a move. (1-9)");
-        int move = input.nextInt();
+        System.out.println("Please make a move.Column (1-" + NUMBER_OF_SIDES + ")");
+        int moveColumn = input.nextInt();
 
-        while(!updateBoard(H, move, gameBoard)){
+        System.out.println("Please make a move.Raw (1-" + NUMBER_OF_SIDES + ")");
+        int moveRaw = input.nextInt();
+
+
+        while (!updateBoard(H, moveColumn, moveRaw, gameBoard)) {
             System.out.println("Sorry! Invalid Move. Try again");
-            move = input.nextInt();
+            moveColumn = input.nextInt();
+            moveRaw = input.nextInt();
+
         }
     }
 
     private static void computerMove(char[][] gameBoard) {
         Random rand = new Random();
-        while (!updateBoard(C, rand.nextInt(9) + 1, gameBoard)) {
+        while (!updateBoard(C, rand.nextInt(NUMBER_OF_SIDES) + 1, rand.nextInt(NUMBER_OF_SIDES) + 1, gameBoard)) {
 //            System.out.println("computer moved to" + move);
         }
     }
 
     private static boolean winOrLoseOrDraw(char[][] gameBoard) {
-        boolean wasThereAWin = true;
-//                checkDiagonalsForWin(gameBoard)
-//                && checkHorizontalsForWin(gameBoard)
-//                && checkVerticalsForWin();
-        return wasThereAWin || itIsADrawNoMoreSpotsLeftNoWinenrs(gameBoard);
-//        add game over logic and return true
-//        for (char[] row : gameBoard) {
-//            for (char c : row) {
-//                if (c == '-') return false;
-//
-//            }
-//        }
-//
-//        // lets define if there is a winner
-//
-//        if (((gameBoard[0][0] == gameBoard[1][1]) && (gameBoard[0][0] == gameBoard[2][2])) ||
-//                ((gameBoard[0][0] == gameBoard[1][0]) && (gameBoard[0][0] == gameBoard[2][0])) ||
-//                ((gameBoard[0][0] == gameBoard[0][1]) && (gameBoard[0][0] == gameBoard[0][2])) ||
-//                ((gameBoard[1][0] == gameBoard[1][1]) && (gameBoard[1][0] == gameBoard[1][2])) ||
-//                ((gameBoard[2][0] == gameBoard[1][1]) && (gameBoard[2][0] == gameBoard[0][2])) ||
-//                ((gameBoard[2][0] == gameBoard[2][1]) && (gameBoard[2][0] == gameBoard[2][2])) ||
-//                ((gameBoard[0][1] == gameBoard[1][1]) && (gameBoard[0][1] == gameBoard[2][1])) ||
-//                ((gameBoard[0][2] == gameBoard[1][2]) && (gameBoard[0][2] == gameBoard[2][2]))
-//        ) {
-//            System.out.println("HURREY WE HAVE A WINNER");
-//        }
-//        return true;
+        return checkColumnsAndRawsAndDiagonalsForWin(gameBoard) || itNoMoreSpotsLeftNoWinners(gameBoard);
     }
 
-    private static boolean itIsADrawNoMoreSpotsLeftNoWinenrs(char[][] gameBoard) {
+    private static boolean checkColumnsAndRawsAndDiagonalsForWin(char[][] gameBoard) {
+        for (int i = 0; i < NUMBER_OF_SIDES; i++) {
+            if (checkForColumns(gameBoard, i)) return true;
+            if (checkForRaws(gameBoard, i)) return true;
+        }
+        return checkDiagonalsForWin(gameBoard);
+    }
+
+    private static boolean checkDiagonalsForWin(char[][] gameBoard) {
+        Set<Character> charSet = new HashSet<>();
+        for (int i = 0; i < NUMBER_OF_SIDES; i++) {
+            charSet.add(gameBoard[i][i]);
+        }
+        if (analyseRawColDiagSetData(charSet, "DIAGONALS ")) return true;
+
+        for (int i = NUMBER_OF_SIDES - 1; i >= 0; i--) {
+            charSet.add(gameBoard[NUMBER_OF_SIDES - 1 - i][i]);
+        }
+        return analyseRawColDiagSetData(charSet, "DIAGONALS ");
+
+    }
+
+    private static boolean checkForRaws(char[][] gameBoard, int i) {
+        Set<Character> charSet = new HashSet<>();
+
+        for (int j = 0; j < NUMBER_OF_SIDES; j++) {
+            charSet.add(gameBoard[j][i]);
+        }
+        return analyseRawColDiagSetData(charSet, "RAWS");
+    }
+
+    private static boolean checkForColumns(char[][] gameBoard, int i) {
+        Set<Character> charSet = new HashSet<>();
+        for (int j = 0; j < NUMBER_OF_SIDES; j++) {
+            charSet.add(gameBoard[i][j]);
+        }
+        return analyseRawColDiagSetData(charSet, "COLUMNS");
+    }
+
+    private static boolean analyseRawColDiagSetData(Set<Character> charSet, String whathasWon) {
+        if (charSet.size() == 1 && !charSet.contains('-')) {
+            System.out.println("HURREY WE HAD A WINNER it is " + charSet.toString() + " COLUMNS " + whathasWon);
+            return true;
+        }
+        charSet.clear();
+        return false;
+    }
+
+
+    private static boolean itNoMoreSpotsLeftNoWinners(char[][] gameBoard) {
         for (char[] row : gameBoard) {
             for (char c : row) {
                 if (c == '-') return false;
 
             }
         }
+        System.out.println("  -----  BOARD IS FULL  ----  ");
         return true;
     }
 
-    private static boolean updateBoard(char type, int position, char[][] gameBoard) {
+    private static boolean updateBoard(char type, int column, int raw, char[][] gameBoard) {
 
-        System.out.println("move coming from " + type + " to position " + position);
-
-        return switch (position) {
-            case 1:
-                yield putPlayerMoveOnBoard(gameBoard, 0,0, type);
-            case 2:
-                yield putPlayerMoveOnBoard(gameBoard, 1,0, type);
-            case 3:
-                yield putPlayerMoveOnBoard(gameBoard, 2,0, type);
-            case 4:
-                yield putPlayerMoveOnBoard(gameBoard, 0,1, type);
-            case 5:
-                yield putPlayerMoveOnBoard(gameBoard, 1,1, type);
-            case 6:
-                yield putPlayerMoveOnBoard(gameBoard, 2,1, type);
-            case 7:
-                yield putPlayerMoveOnBoard(gameBoard, 0,2, type);
-            case 8:
-                yield putPlayerMoveOnBoard(gameBoard, 1,2, type);
-            case 9:
-                yield putPlayerMoveOnBoard(gameBoard, 2,2, type);
-            default:
-                System.out.println("wrong value");
-                yield false;
-        };
-
+        System.out.println("move coming from " + type + " to column " + column + " to raw " + raw);
+        if (column > 0 && column <= NUMBER_OF_SIDES && raw > 0 && raw <= NUMBER_OF_SIDES) {
+            return putPlayerMoveOnBoard(gameBoard, column, raw, type);
+        } else {
+            System.out.println("wrong value");
+            return false;
+        }
     }
 
-    private static boolean putPlayerMoveOnBoard(char[][] gameBoard, int y, int x, char type) {
-        if (gameBoard[y][x] == '-') {
-            gameBoard[y][x] = type;
+    private static boolean putPlayerMoveOnBoard(char[][] gameBoard, int column, int raw, char type) {
+        if (gameBoard[column - 1][raw - 1] == '-') {
+            gameBoard[column - 1][raw - 1] = type;
             return true;
         } else {
-            System.out.println(" " + y + " " + x + " is already busy please retry the move" );
+            System.out.println(" " + column + " " + raw + " is already busy please retry the move");
             return false;
         }
     }
